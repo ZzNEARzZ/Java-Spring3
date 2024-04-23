ddfb9b10359da1fab43cd56b937a632921b75ccf
JESUS JHOJANSON BERMEJO LLANOS <jhonbermejo4@gmail.com>
2024-04-22 22:39:00 -0500

    Anotaciones con Query Methods

--- SECCION9_SPRING BOOT3_JPA-HIBERNATE/spring/springboot-jpa/src/main/java/com/jhon/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
+++ SECCION9_SPRING BOOT3_JPA-HIBERNATE/spring/springboot-jpa/src/main/java/com/jhon/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
@@ -1,11 +1,20 @@
 package com.jhon.curso.springboot.jpa.springbootjpa;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.jhon.curso.springboot.jpa.springbootjpa.entities.Person;
+import com.jhon.curso.springboot.jpa.springbootjpa.repositories.PersonRepository;
+
 @SpringBootApplication
 public class SpringbootJpaApplication implements CommandLineRunner{
+
+	@Autowired
+	private PersonRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaApplication.class, args);
@@ -13,6 +22,14 @@ public class SpringbootJpaApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		//List<Person> persons = (List<Person>) repository.findAll();
+		//List<Person> persons = (List<Person>) repository.buscarByProgrammingLanguage("Java","Maria");
+		List<Person> persons = (List<Person>) repository.findByProgrammingLanguageAndName("Java","Andres");
+
+		persons.stream().forEach(person -> {
+			System.out.println(person);
+		});
 		
 	}
 
---
+++ SECCION9_SPRING BOOT3_JPA-HIBERNATE/spring/springboot-jpa/src/main/java/com/jhon/curso/springboot/jpa/springbootjpa/repositories/PersonRepository.java
@@ -0,0 +1,19 @@
+package com.jhon.curso.springboot.jpa.springbootjpa.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.jhon.curso.springboot.jpa.springbootjpa.entities.Person;
+
+public interface PersonRepository extends CrudRepository<Person, Long>{
+
+    List<Person> findByProgrammingLanguage(String programmingLanguage);
+
+    @Query("select p from Person p where p.programmingLanguage=?1 and p.name=?2")
+    List<Person> buscarByProgrammingLanguage(String programmingLanguage, String name);
+
+    List<Person> findByProgrammingLanguageAndName(String programmingLanguage, String name);
+
+}
